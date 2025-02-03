55302071dc83c5dbd451c9b3356b1646c2d51ef0
Shariar Hossain Riad <76766167+shariar-hriad@users.noreply.github.com>
2025-02-03 20:40:59 +0600

    feat: add skeleton loading cards to dashboard layout

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,34 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
     title: 'Dashboard',
 }
 
+const DummyCard = ({ title }: { title: string }) => (
+    <Card>
+        <CardHeader>
+            <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <div className='space-y-2'>
+                <Skeleton className='h-4 w-[250px]' />
+                <Skeleton className='h-4 w-[200px]' />
+                <Skeleton className='h-4 w-[150px]' />
+            </div>
+        </CardContent>
+    </Card>
+)
+
 export default function Dashboard() {
     return (
         <div className='space-y-5'>
             <div className='grid items-start grid-cols-1 md:grid-cols-2 gap-5'>
-                Dashboard
+                <DummyCard title='Total Products' />
+                <DummyCard title='Total Orders' />
+                <DummyCard title='Total Revenue' />
+                <DummyCard title='Total Customers' />
             </div>
         </div>
     )
